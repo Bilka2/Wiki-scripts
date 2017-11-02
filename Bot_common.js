1a58b4e71bbeec7062c2c740be5e75cb54ef09c9
Bilka <Bilka2@users.noreply.github.com>
2017-11-02 12:31:31 +0100

    Moved redirect stuff to bilkabot only script

--- Bot_common.js
+++ Bot_common.js
@@ -703,6 +703,252 @@ function updateEntityHealthInInfobox(entity) {
 }
 
 
+/* Redirects -> Factorio:Redirects */
+
+$("#create-redirect-list").click(function(){
+	getToken();
+	createRedirectList();
+});
+
+function createRedirectList() {
+	getUserGroup();
+	if (userGroup.some(isBot) == false) return;
+	getRedirects();
+};
+
+function getRedirects() {
+	var redirects = [];
+
+	$.ajax({
+		url: apiUrl,
+		data: {
+			format: 'json',
+			action: 'query',
+			list: 'allpages',
+			aplimit: '5000',
+			apfilterredir: 'redirects',
+		},
+		async: false,
+		dataType: 'json',
+		type: 'GET',
+		success: function( data ) {
+			redirects = data.query.allpages;
+		},
+		error: function( xhr ) {
+			alert( 'Error: Allpages request failed.' );
+		}
+	});
+  
+	$.ajax({
+		url: apiUrl,
+		data: {
+			format: 'json',
+			action: 'query',
+			list: 'allpages',
+			aplimit: '5000',
+			apfilterredir: 'redirects',
+			apnamespace: '6'
+		},
+		async: false,
+		dataType: 'json',
+		type: 'GET',
+		success: function( data ) {
+			for (var i = 0; i < data.query.allpages.length; i++) {
+				redirects.push(data.query.allpages[i])
+			}
+		},
+		error: function( xhr ) {
+			alert( 'Error: Allpages request failed.' );
+		}
+	});
+  
+	for (var i = 0; i < redirects.length; i++) {
+		var backlinks = getBacklinks(redirects[i].title);
+		if (redirects[i].title.search(/File:/i) == 0) {
+			var imageusage = getFileUsage(redirects[i].title);
+			//don't put imageusage into backlinks if it's already there:
+			jloop1: for (var j = 0; j < imageusage.length; j++) {
+				for (var k = 0; k < backlinks.length; k++) {
+					if (backlinks[k].title == imageusage[j].title) {
+						continue jloop1;
+					}
+				}
+				backlinks.push(imageusage[j]);
+			}
+		}
+		redirects[i].value = backlinks.length;  
+	}
+	redirects = redirects.sort(compare);
+	createRedirectsPage("Factorio:Redirects", redirects);
+};
+
+function createRedirectsPage(loc, redirects) {
+	var formattedRedirects = "{|class=wikitable\n!#\n!Redirect\n!Links to this redirect";
+	for (var i = 0; i < redirects.length; i++) {    
+		formattedRedirects = formattedRedirects.concat("\n|-\n|" + (i + 1) + "\n|[https://wiki.factorio.com/index.php?title=" + encodeURI(redirects[i].title) + "&redirect=no " + redirects[i].title + "]\n|[https://wiki.factorio.com/index.php?title=Special:WhatLinksHere/" + encodeURI(redirects[i].title) + " " + redirects[i].value + "]");
+	}
+	formattedRedirects = formattedRedirects.concat("\n|}");
+	createPage(loc, formattedRedirects, "Updated the list of redirects.");
+};
+
+function lowercaseFirstLetter(string) {
+	return string.charAt(0).toLowerCase() + string.slice(1);
+};
+
+/* Change links pointing to a redirect */
+
+$("#change-links-to-redirect").click(function(){
+	getRedirectToChange();
+});
+
+function getRedirectToChange() {
+	getUserGroup();
+	if (userGroup.some(isBot) == false) return;
+	var redirect = prompt("Please enter the redirect. Pages linking to it will have their links changed to go to the link the redirect links to. So make sure it links to the right page!");
+	if (!redirect) return;
+	getToken();
+	changeLinksToRedirect(redirect);
+};
+
+function changeLinksToRedirect(redirect) {
+	var redirectLinks = [];
+	var newLink = "";
+	$.ajax({
+		url: apiUrl,
+		data: {
+			format: 'json',
+			action: 'query',
+			titles: redirect,
+			redirects: ''
+		},
+		async: false,
+		dataType: 'json',
+		type: 'GET',
+		success: function( data ) {
+			if (data.query.redirects) {
+				newLink = data.query.redirects[0].to;
+			} else {
+				alert("Error! The page is not a redirect.");
+				return;
+			}
+			if (data.query.normalized) {
+				redirectLinks[0] = data.query.normalized[0].from;
+				redirectLinks[1] = data.query.normalized[0].to;
+			} else {
+				redirectLinks[0] = redirect;
+				if (redirect.replace(/\s/g, "_") != redirect) redirectLinks.push(redirect.replace(" ", "_"));
+			}
+			var backlinks = getBacklinks(redirect);
+			var fileLink = false;
+			if (redirectLinks[0].search(/File:/i) == 0) {
+				fileLink = true;
+				var imageusage = getFileUsage(redirect);
+				//don't put imageusage into backlinks if it's already there:
+				iloop3: for (var i = 0; i < imageusage.length; i++) {
+					for (var j = 0; j < backlinks.length; j++) {
+						if (backlinks[j].title == imageusage[i].title) {
+							continue iloop3;
+						}
+					}
+				backlinks.push(imageusage[i]);
+				}
+			}
+			if (backlinks.length == 0) {
+				alert("Nothing links there.")
+				return;
+			}
+	
+			var newNotFileLink = "";
+			if (fileLink) {
+				newNotFileLink = newLink.slice(5, -4);
+				for (var i = 0; i < redirectLinks.length; i++) {
+					if (redirectLinks[i].search(/File:/i) == 0) redirectLinks.push(redirectLinks[i].slice(5, -4));
+				}
+			}
+			for (var i = 0; i < redirectLinks.length; i++) {
+				var lowercaseLink = lowercaseFirstLetter(redirectLinks[i]);
+				if (lowercaseLink != redirectLinks[i]) redirectLinks.push(lowercaseLink); //possible duplicates don't really matter here
+			}
+			console.log(redirectLinks)
+		
+			for (var i = 0; i < backlinks.length; i++) {
+				$.ajax({
+					url: apiUrl,
+					data: {
+						format: 'json',
+						action: 'query',
+						titles: backlinks[i].title,
+						prop: 'revisions',
+						rvprop: 'content'
+					},
+					async: false,
+					dataType: 'json',
+					type: 'GET',
+					success: function( data ) {
+						var pages = data.query.pages;
+						var revisions = pages[Object.keys(pages)[0]].revisions[0];
+						var oldContent = revisions[Object.keys(revisions)[2]];
+						var content = oldContent;
+						for (var j = 0; j < redirectLinks.length; j++) {
+							while (true) {
+								var tempcontent = "";
+								var tempNewLink = "";
+								if (fileLink) {
+									tempNewLink = redirectLinks[j].search(/File:/i) == 0 ? newLink : newNotFileLink; //var value = *condition* ? *true* : *false*;
+								} else {
+									tempNewLink = newLink;
+								}
+								tempcontent = content.replace("[[" + redirectLinks[j] + "]]", "[[" + tempNewLink + "]]"); //gotta love being unable to compile a reg expr
+								tempcontent = tempcontent.replace("[[" + redirectLinks[j] + " ]]", "[[" + tempNewLink + "]]");
+								tempcontent = tempcontent.replace("[[" + redirectLinks[j] + "#", "[[" + tempNewLink + "#");
+								tempcontent = tempcontent.replace("[[" + redirectLinks[j] + "|", "[[" + tempNewLink + "|");
+								tempcontent = tempcontent.replace("[[" + redirectLinks[j] + " |", "[[" + tempNewLink + "|");
+								tempcontent = tempcontent.replace("[[" + redirectLinks[j] + "  ", "[[" + tempNewLink + "  ");
+								tempcontent = tempcontent.replace("{{Icon|" + redirectLinks[j] + "|", "{{Icon|" + tempNewLink + "|");
+								tempcontent = tempcontent.replace("{{Icon|" + redirectLinks[j] + " |", "{{Icon|" + tempNewLink + "|");
+								tempcontent = tempcontent.replace("{{icon|" + redirectLinks[j] + "|", "{{Icon|" + tempNewLink + "|");
+								tempcontent = tempcontent.replace("{{icon|" + redirectLinks[j] + " |", "{{Icon|" + tempNewLink + "|");
+								tempcontent = tempcontent.replace("{{Imagelink|" + redirectLinks[j] + "|", "{{Imagelink|" + tempNewLink + "|");
+								tempcontent = tempcontent.replace("{{Imagelink|" + redirectLinks[j] + " |", "{{Imagelink|" + tempNewLink + "|");
+								tempcontent = tempcontent.replace("{{imagelink|" + redirectLinks[j] + "|", "{{Imagelink|" + tempNewLink + "|");
+								tempcontent = tempcontent.replace("{{imagelink|" + redirectLinks[j] + " |", "{{Imagelink|" + tempNewLink + "|");
+								if (!fileLink) {
+									tempcontent = tempcontent.replace("|" + redirectLinks[j] + "}}", "|" + tempNewLink + "}}"); //I hope this doesn't break anything
+									//tempcontent = tempcontent.replace("|" + redirectLinks[j] + "|", "|" + tempNewLink + "|"); //I hope this doesn't break anything
+								} else { //only done for file links
+									tempcontent = tempcontent.replace("[[:" + redirectLinks[j] + "]]", "[[:" + tempNewLink + "]]");
+									tempcontent = tempcontent.replace("[[:" + redirectLinks[j] + " ]]", "[[:" + tempNewLink + "]]");
+									tempcontent = tempcontent.replace("[[:" + redirectLinks[j] + "|", "[[:" + tempNewLink + "|");
+									tempcontent = tempcontent.replace("[[:" + redirectLinks[j] + " |", "[[:" + tempNewLink + "|");
+									tempcontent = tempcontent.replace("[[:" + redirectLinks[j] + "  ", "[[:" + tempNewLink + "  ");
+								}
+								if (tempcontent != content) {
+									content = tempcontent;
+								} else {
+									break;
+								}
+							}
+						}
+						if (oldContent != content) {
+							genericEditPage(backlinks[i].title, content, "Changed links going to the redirect " + redirectLinks[0] + " to go to " + newLink + ".")
+						} else {
+							console.log("Could not correct the links on " + backlinks[i].title + " using code. Sorry about that.");
+						}
+					},
+					error: function( xhr ) {
+						alert( 'Error: Get content failed.' );
+					}
+				});
+			}
+			console.log("Done!");
+		},
+		error: function( xhr ) {
+			alert( 'Error: Request failed.' );
+		}
+	});
+};
+
+
 /* get the number of users who ever made an edit, for conveniece */
 
 $("#GetEditingUsers").click(function(){
