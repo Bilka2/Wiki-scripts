815196305e8147c390b14d3661c70f80393e99f3
Bilka <Bilka2@users.noreply.github.com>
2018-03-28 17:28:44 +0200

    Getting pages works now

--- Find-outdated-lang-pages-WIP.js
+++ Find-outdated-lang-pages-WIP.js
@@ -1,4 +1,4 @@
-var languages = {
+var languages = [
 	{suffix:"cs", name:"Czech"},
 	{suffix:"de", name:"German"},
 	{suffix:"es", name:"Spanish"},
@@ -17,16 +17,29 @@ var languages = {
 	{suffix:"ms", name:"Malay"},
 	{suffix:"da", name:"Danish"},
 	{suffix:"hu", name:"Hungarian"}
-};
+];
 
 function main(languages) {
 	var englishPages = getCategorymembers('Category:English page');
-	//getPageRevisions(englishPages, false);
-	var englishPagesRevisions = getLastEditDateByPageArray(englishPages.join('|'));
+	var englishPagesRevisions = getPageRevisions(englishPages, false);
+	var langPages = {};
 	for (var i = 0; i < languages.length; i++) {
-		var langPages = getCategorymembers('Category:' + languages[i].name + ' page');
-		var langPagesRevisions = getLastEditDateByPageArray(langPages.join('|'));
+		var pages = getCategorymembers('Category:' + languages[i].name + ' page');
+		var pagesRevisions = getPageRevisions(langPages, true);
+		langPages[languages[i].suffix] = {pages: pages, pagesRevisions: pagesRevisions, name: languages[i].name};
 	}
+	for (var key in langPages) {
+		if (!langPages.hasOwnProperty(key)) continue;
+		var value = langPages[key];
+		//here
+	}
+};
+
+function test() {
+	var englishPages = getCategorymembers('Category:English page');
+	var englishPagesRevisions = getPageRevisions(englishPages, false);
+	console.log(englishPages);
+	console.log(englishPagesRevisions);
 };
 
 function getCategorymembers(category) {
@@ -47,9 +60,9 @@ function getCategorymembers(category) {
 		success: function( data ) {
 			var categorymembers = data.query.categorymembers;
 			console.log('Found ' + categorymembers.length + ' pages in ' + category);
-			for (var i = 0; i < categorymembers.length; i++) {
-				array.push(categorymembers[i].title);
-			}
+			categorymembers.forEach(function(page){
+				array.push(page.title)
+			});
 		},
 		error: function( xhr ) {
 			alert( 'Error: Request failed.' );
@@ -59,23 +72,21 @@ function getCategorymembers(category) {
 };
 
 function getPageRevisions(array, removeRevisionsByBilka) {
-	var pageRevisions = getPageRevisionsByPages(array.join('|'));
+	var pageRevisions = staggerGetPageRevisionsByPages(array); //We timeout or something if we query too many pages
 	if (removeRevisionsByBilka) {
-		for (var i = 0; i < pageRevisions.length; i++) {
-			var rev = getLatestRevisionNotByBilka(pageRevisions[i]);
-			if (rev !== pageRevisions[i]) {
-				//here
-			}
-		}
+		for (var i = 0; i < pageRevisions.length; i++) pageRevisions[i] = getLatestRevisionNotByBilka(pageRevisions[i]);
 	}
+	return pageRevisions;
 };
 
-function getLatestRevisionNotByBilka(pageRevisions) {
-	var revisions = pageRevisions;
-	while (revisions.user.includes("Bilka") && revisions.parentid != 0) {
-		revisions = getPageRevisionsByRevID(revisions.parentid);
+function staggerGetPageRevisionsByPages(array) {
+	var resultArray = [];
+	for (var i = 0; i < array.length; i += 200) {
+		var arrayPart = array.slice(i, i + 200);
+		if (i == 0) resultArray = getPageRevisionsByPages(arrayPart.join('|'));
+		else resultArray = resultArray.concat(getPageRevisionsByPages(arrayPart.join('|')));
 	}
-	return revisions;
+	return resultArray;
 };
 
 function getPageRevisionsByPages(pages) {
@@ -94,17 +105,27 @@ function getPageRevisionsByPages(pages) {
 		type: 'GET',
 		success: function( data ) {
 			var pages = data.query.pages;
-			for (var i = 0; i < pages.length; i++) {
-				if (pages[i].missing) continue;
-				var pageRevisions = new pageRevisions(pages[i].title, pages[i].revisions[0].user, pages[i].revisions[0].timestamp, pages[i].revisions[0].revid, pages[i].revisions[0].parentid);
+			for (var key in pages) {
+				if (!pages.hasOwnProperty(key)) continue;
+				var page = pages[key];
+				if (page.missing) continue;
+				var pageRevisions = pageRevisionsFromAPIResponse(page);
 				array.push(pageRevisions);
 			}
 		},
 		error: function( xhr ) {
-			alert( 'Error: Request failed.' );
+			alert( 'Error: Request failed. ' + xhr.status + ': ' + xhr.statusText);
 		}
 	});
 	return array;
+};
+
+function getLatestRevisionNotByBilka(pageRevisions) {
+	var revisions = pageRevisions;
+	while (revisions.user.includes("Bilka") && revisions.parentid != 0) {
+		revisions = getPageRevisionsByRevID(revisions.parentid);
+	}
+	return revisions;
 };
 
 function getPageRevisionsByRevID(revid) {
@@ -123,7 +144,7 @@ function getPageRevisionsByRevID(revid) {
 		success: function( data ) {
 			var pages = data.query.pages;
 			var pageInfo = pages[Object.keys(pages)[0]];
-			var pageRevisions = new pageRevisions(pageInfo.title, pageInfo.revisions[0].user, pageInfo.revisions[0].timestamp, pageInfo.revisions[0].revid, pageInfo.revisions[0].parentid);
+			var pageRevisions = pageRevisionsFromAPIResponse(pageInfo);
 			return pageRevisions;
 		},
 		error: function( xhr ) {
@@ -132,6 +153,9 @@ function getPageRevisionsByRevID(revid) {
 	});
 };
 
+function pageRevisionsFromAPIResponse(page) {
+	return new pageRevisions(page.title, page.revisions[0].user, page.revisions[0].timestamp, page.revisions[0].revid, page.revisions[0].parentid);
+};
 
 function pageRevisions(title, user, timestamp, revid, parentid) {
   this.title = title;
@@ -139,4 +163,4 @@ function pageRevisions(title, user, timestamp, revid, parentid) {
   this.timestamp = timestamp;
   this.revid = revid;
   this.parentid = parentid;
-}
+};
