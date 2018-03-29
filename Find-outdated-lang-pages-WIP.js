6290bd5ffd1e04e0aebeb0eabd67f3caecdba42a
Bilka <Bilka2@users.noreply.github.com>
2018-03-29 19:58:56 +0200

    Everything works

--- Find-outdated-lang-pages-WIP.js
+++ Find-outdated-lang-pages-WIP.js
@@ -7,7 +7,7 @@ var languages = [
 	{suffix:"ja", name:"Japanese"},
 	{suffix:"nl", name:"Dutch"},
 	{suffix:"pl", name:"Polish"},
-	{suffix:"pt-br", name:"Portuguese"},
+	{suffix:"pt-br", name:"Brazilian Portuguese"},
 	{suffix:"ru", name:"Russian"},
 	{suffix:"sv", name:"Swedish"},
 	{suffix:"uk", name:"Ukrainian"},
@@ -22,22 +22,43 @@ var languages = [
 function main(languages) {
 	var englishPagesRevisions = getPageRevisions(getCategorymembers('Category:English page'), false);
 	var langPages = {};
-	for (var i = 0; i < /*languages.length*/ 1 ; i++) {
-		var pagesRevisions = getPageRevisions(getCategorymembers('Category:' + languages[i].name + ' page'), true);
-		langPages[languages[i].suffix] = {pagesRevisions: pagesRevisions, name: languages[i].name};
+	for (var i = 0; i < languages.length ; i++) {
+		var pages = getCategorymembers('Category:' + languages[i].name + ' page')
+		var pagesRevisions = getPageRevisions(pages, true);
+		langPages[languages[i].suffix] = {pagesRevisions: pagesRevisions, name: languages[i].name, overallPages: pages};
 	}
 	for (var suffix in langPages) {
 		if (!langPages.hasOwnProperty(suffix)) continue;
 		var value = langPages[suffix];
 		var langPageRevisionsList = value.pagesRevisions;
+		var outdatedPages = [];
 		for (var langPageName in langPageRevisionsList) {
 			if (!langPageRevisionsList.hasOwnProperty(langPageName)) continue;
 			var langPageRevisions = langPageRevisionsList[langPageName];
-			isLangPageOutdated(langPageRevisions, suffix, englishPagesRevisions);
-			//put in array/object
-			//print outdated pages
+			var outdated = isLangPageOutdated(langPageRevisions, suffix, englishPagesRevisions);
+			if (outdated) outdatedPages.push(langPageRevisions);
 		}
+		langPages[suffix].outdatedPages = outdatedPages;
 	}
+	var output = '';
+	for (var suffix in langPages) {
+		if (!langPages.hasOwnProperty(suffix)) continue;
+		var value = langPages[suffix];
+		var overallPages = value.overallPages;
+		var outdatedPages = value.outdatedPages;
+		outdatedPages = outdatedPages.sort(comparePageRevisions);
+		var outdatedPercent = outdatedPages.length / overallPages.length * 100;
+		outdatedPercent = Math.round(outdatedPercent);
+		output = output.concat('== ' + value.name + ' ==\n' + outdatedPages.length + ' out of ' + overallPages.length + ' (' + outdatedPercent + '%) ' + value.name + ' pages are outdated.\n' + '{|class=wikitable\n!Page\n!Last edited\n!English page last edited');
+		outdatedPages.forEach(function(langPageRevisions){
+			var en = getEnglishPageRevisions(langPageRevisions.title, suffix, englishPagesRevisions);
+			var enTimestamp = en ? en.timestamp : '---';
+			output = output.concat('\n|-\n|[' + mw.config.get('wgServer') + '/index.php?title=' + encodeURI(langPageRevisions.title) + ' ' + langPageRevisions.title + ']\n| ' + readableDate(langPageRevisions.timestamp) + '\n| ' + readableDate(enTimestamp));
+		});
+		output = output.concat('\n|}\n');
+	}
+	getToken();
+	createPage('User:Bilka/Outdated pages', output, 'Updated the list of outdated language pages.');
 };
 
 function getCategorymembers(category) {
@@ -71,9 +92,9 @@ function getCategorymembers(category) {
 
 function getPageRevisions(array, removeRevisionsByBilka) {
 	var pageRevisionsList = staggerGetPageRevisionsByPages(array); //We timeout or something if we query too many pages
-	if (removeRevisionsByBilka) {
-		for (var i = 0; i < pageRevisionsList.length; i++) pageRevisionsList[i] = getLatestRevisionNotByBilka(pageRevisionsList[i]);
-	}
+	if (removeRevisionsByBilka)
+		for (var i = 0; i < pageRevisionsList.length; i++)
+			pageRevisionsList[i] = getLatestRevisionNotByBilka(pageRevisionsList[i]);
 	//turn array into object indexed by page name to make it easier to find the properties of a page
 	var pageRevisionsObject = {};
 	pageRevisionsList.forEach(function(pageRevisions) {
@@ -84,8 +105,8 @@ function getPageRevisions(array, removeRevisionsByBilka) {
 
 function staggerGetPageRevisionsByPages(array) {
 	var resultArray = [];
-	for (var i = 0; i < array.length; i += 200) {
-		var arrayPart = array.slice(i, i + 200);
+	for (var i = 0; i < array.length; i += 100) {
+		var arrayPart = array.slice(i, i + 100);
 		if (i == 0) resultArray = getPageRevisionsByPages(arrayPart.join('|'));
 		else resultArray = resultArray.concat(getPageRevisionsByPages(arrayPart.join('|')));
 	}
@@ -117,7 +138,7 @@ function getPageRevisionsByPages(pages) {
 			}
 		},
 		error: function( xhr ) {
-			alert( 'Error: Request failed. ' + xhr.status + ': ' + xhr.statusText);
+			alert( 'Error: Request failed. ' + xhr.status + ': ' + xhr.statusText + '\n Requested pages: ' + pages);
 		}
 	});
 	return array;
@@ -160,10 +181,7 @@ function getPageRevisionsByRevID(revid) {
 function isLangPageOutdated(langPageRevisions, suffix, englishPagesRevisions) {
 	var langTimestamp = langPageRevisions.timestamp;
 	var en = getEnglishPageRevisions(langPageRevisions.title, suffix, englishPagesRevisions);
-	if (!en) {
-		console.log('No page ' + getEnglishPageName(langPageRevisions.title, suffix))
-		return true; // Outdated	
-	}
+	if (!en) return true; // Outdated	
 	/*console.log('en: ' + en.timestamp + ', ' + suffix + ': ' + langTimestamp);
 	if (en.timestamp < langTimestamp) console.log('en.timestamp < langTimestamp ' + langPageRevisions.title);
 	if (en.timestamp > langTimestamp) console.log('en.timestamp > langTimestamp ' + langPageRevisions.title);*/
@@ -189,4 +207,24 @@ function pageRevisions(title, user, timestamp, revid, parentid) {
   this.timestamp = timestamp;
   this.revid = revid;
   this.parentid = parentid;
+};
+
+function readableDate(datestring) {
+	var date = new Date(datestring);
+	var options = {  
+		year: 'numeric', month: 'short', day: 'numeric', hour12: false, hour: '2-digit', minute: '2-digit', timeZone: 'UTC', timeZoneName: 'short'
+	};
+	return date.toLocaleTimeString('en-us', options);
+};
+
+function comparePageRevisions(a,b) {
+	if (parseInt(a.timestamp) > parseInt(b.timestamp))
+		return -1;
+	if (parseInt(a.timestamp) < parseInt(b.timestamp))
+		return 1;
+	if (a.title < b.title)
+		return -1;
+	if (a.title > b.title)
+		return 1;
+	return 0;
 };
