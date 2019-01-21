59b6e56aaeef99fe440477d2098dd47d8f5ee736
Bilka <Bilka2@users.noreply.github.com>
2019-01-21 17:48:05 +0100

    Moved misc python scripts all into one file

---
+++ misc_scripts.py
@@ -0,0 +1,92 @@
+## A collection of miscellaneous one-off scripts ##
+
+import json
+import re
+import requests
+from util import get_edit_token, get_page, edit_page
+api_url = 'https://wiki.factorio.com/api.php'
+
+
+def used_as_ammo_by_in_infobox(used_by, page):
+  page = "Infobox:" + page
+  session = requests.Session()
+  edit_token = get_edit_token(session, api_url)
+  page_data = get_page(session, api_url, page)
+  infobox_start = page_data.find("|")
+  used_by_string = " + ".join(used_by)
+  new_page_data = page_data[:infobox_start] + f"|used-as-ammo-by = {used_by_string}\n" + page_data[infobox_start:]
+  print(new_page_data)
+  print(edit_page(session, api_url, edit_token, page, new_page_data, "Added \"Usable Ammunition For\" to infobox").text)
+
+
+def infobox_categoy_change(pages, category): # regexr.com/3trgo
+  session = requests.Session()
+  edit_token = get_edit_token(session, api_url)
+  for page in pages:
+    page_data = get_page(session, api_url, page)
+    page_data = re.sub(r'\|\s*category\s*=([^\n])+\n', f'|category = {category}\n', page_data)
+    if 'category-name' in page_data:
+      page_data = re.sub(r'\|\s*category-name([^\n])+\n', '', page_data)
+    print(page_data)
+    print(edit_page(session, api_url, edit_token, page, page_data, "New infobox categories").text)
+
+
+def make_type():
+  name = input('name ')
+  type = input('type ')
+  default = input('default ')
+  
+  out = '=== ' + name + ' ===\n\'\'\'Type\'\'\': [[Types/' + type + ']]\n'
+  
+  if default:
+    out += '\n\'\'\'Default\'\'\': ' + default + '\n'
+  
+  print(out)
+
+
+def check_if_all_prototypes_are_on_page():
+  with open('C:\\Users\\Erik\\Documents\\Factorio\\prototype_types_the_game_will_respond_to.txt', 'r') as f: # https://gist.github.com/Bilka2/e1dd885f5ff8eb53d1b66315663f5ca5
+    data = list(f)
+  
+  session = requests.Session()
+  edit_token = get_edit_token(session, api_url)
+  content = get_page(session, api_url, 'Prototype definitions')
+  
+  for line in data:
+    if not ('\'\'\'' + line.strip() + '\'\'\'' in content):
+      print(line.strip())
+
+
+def prototype_types_on_individual_pages():
+  with open('prototype-types.json', 'r') as f:
+    pageToTypeMapping = json.load(f)
+
+  session = requests.Session()
+  edit_token = get_edit_token(session, api_url)
+  for page, type in pageToTypeMapping.items():
+    try:
+      pageData = get_page(session, api_url, page)
+      if "Basics" in pageData or "basics" in pageData:
+        replacement = r"\1Prototype type: '''" + type + "'''\n\n"
+        if type == "<abstract>":
+          replacement = r"\1This type is abstract and cannot be created directly.\n\n"
+        pattern = re.compile(r"(==\s*Basics\s*==\n)", re.IGNORECASE)
+        pageData = re.sub(pattern, replacement, pageData)
+        print(edit_page(session, api_url, edit_token, page, pageData, "Added prototype type to page").text)
+      else: 
+        print(page + " Does not have basics on page")
+    except:
+      print(page + " Something went wrong")
+
+
+if __name__ == '__main__':
+  # used_as_ammp_by_in_infobox(["Flamethrower turret"], "Light oil")
+  
+  # infobox_categoy_change(["Infobox:Artillery shell", "Infobox:Artillery targeting remote", "Infobox:Artillery turret", "Infobox:Atomic bomb", "Infobox:Battery MK1", "Infobox:Battery MK2", "Infobox:Cannon shell", "Infobox:Cluster grenade", "Infobox:Combat shotgun", "Infobox:Defender capsule", "Infobox:Destroyer capsule", "Infobox:Discharge defense", "Infobox:Discharge defense remote", "Infobox:Distractor capsule", "Infobox:Energy shield", "Infobox:Energy shield MK2", "Infobox:Exoskeleton", "Infobox:Explosive cannon shell", "Infobox:Explosive rocket", "Infobox:Explosive uranium cannon shell", "Infobox:Firearm magazine", "Infobox:Flamethrower", "Infobox:Flamethrower ammo", "Infobox:Flamethrower turret", "Infobox:Gate", "Infobox:Grenade", "Infobox:Gun turret", "Infobox:Heavy armor", "Infobox:Land mine", "Infobox:Laser turret", "Infobox:Light armor", "Infobox:Modular armor", "Infobox:Nightvision", "Infobox:Personal laser defense", "Infobox:Personal roboport", "Infobox:Personal roboport MK2", "Infobox:Piercing rounds magazine", "Infobox:Piercing shotgun shells", "Infobox:Pistol", "Infobox:Poison capsule", "Infobox:Portable fusion reactor", "Infobox:Portable solar panel", "Infobox:Power armor", "Infobox:Power armor MK2", "Infobox:Radar", "Infobox:Rocket", "Infobox:Rocket launcher", "Infobox:Rocket silo", "Infobox:Shotgun", "Infobox:Shotgun shells", "Infobox:Slowdown capsule", "Infobox:Stone wall", "Infobox:Submachine gun", "Infobox:Uranium cannon shell", "Infobox:Uranium rounds magazine"], "Combat")
+  
+  make_type()
+  
+  # check_if_all_prototypes_are_on_page()
+  
+  # prototype_types_on_individual_pages()
+  
