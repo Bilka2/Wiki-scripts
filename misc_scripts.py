1bb25e70575f84774ea84cd935470015e68afd6f
Bilka <Bilka2@users.noreply.github.com>
2019-02-08 15:42:14 +0100

    Added data raw parsing

--- misc_scripts.py
+++ misc_scripts.py
@@ -1,6 +1,7 @@
 ## A collection of miscellaneous one-off scripts ##
 
 import json
+import os.path
 import re
 import requests
 from util import get_edit_token, get_page, edit_page
@@ -79,14 +80,32 @@ def prototype_types_on_individual_pages():
       print(page + " Something went wrong")
 
 
+def convert_data_raw(version):
+  with open(os.path.dirname(os.path.abspath(__file__)) + f'/data/{version}/data-raw-tree-{version}.json', 'r') as f:
+    dataRawJson = json.load(f)
+  
+  out = ''
+  
+  for type, names in dataRawJson.items():
+    assert names
+    out += f'== {type} ==\n'
+    out += '<div style="column-count:2;-moz-column-count:2;-webkit-column-count:2">\n'
+    for name in names:
+      out += f'* {name}\n'
+    out += '</div>\n'
+
+  return out
+
+
 if __name__ == '__main__':
   # used_as_ammp_by_in_infobox(["Flamethrower turret"], "Light oil")
   
   # infobox_categoy_change(["Infobox:Artillery shell", "Infobox:Artillery targeting remote", "Infobox:Artillery turret", "Infobox:Atomic bomb", "Infobox:Battery MK1", "Infobox:Battery MK2", "Infobox:Cannon shell", "Infobox:Cluster grenade", "Infobox:Combat shotgun", "Infobox:Defender capsule", "Infobox:Destroyer capsule", "Infobox:Discharge defense", "Infobox:Discharge defense remote", "Infobox:Distractor capsule", "Infobox:Energy shield", "Infobox:Energy shield MK2", "Infobox:Exoskeleton", "Infobox:Explosive cannon shell", "Infobox:Explosive rocket", "Infobox:Explosive uranium cannon shell", "Infobox:Firearm magazine", "Infobox:Flamethrower", "Infobox:Flamethrower ammo", "Infobox:Flamethrower turret", "Infobox:Gate", "Infobox:Grenade", "Infobox:Gun turret", "Infobox:Heavy armor", "Infobox:Land mine", "Infobox:Laser turret", "Infobox:Light armor", "Infobox:Modular armor", "Infobox:Nightvision", "Infobox:Personal laser defense", "Infobox:Personal roboport", "Infobox:Personal roboport MK2", "Infobox:Piercing rounds magazine", "Infobox:Piercing shotgun shells", "Infobox:Pistol", "Infobox:Poison capsule", "Infobox:Portable fusion reactor", "Infobox:Portable solar panel", "Infobox:Power armor", "Infobox:Power armor MK2", "Infobox:Radar", "Infobox:Rocket", "Infobox:Rocket launcher", "Infobox:Rocket silo", "Infobox:Shotgun", "Infobox:Shotgun shells", "Infobox:Slowdown capsule", "Infobox:Stone wall", "Infobox:Submachine gun", "Infobox:Uranium cannon shell", "Infobox:Uranium rounds magazine"], "Combat")
   
-  make_type()
+  # make_type()
   
   # check_if_all_prototypes_are_on_page()
   
   # prototype_types_on_individual_pages()
   
+  print(convert_data_raw('0.16.51'))
