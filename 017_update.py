a95a739746d9037d0274c8d61a65f199e2f67221
Bilka <Bilka2@users.noreply.github.com>
2019-02-26 15:16:14 +0100

    Finished 0.17 update script

--- 017_update.py
+++ 017_update.py
@@ -4,7 +4,7 @@ import requests
 from util import get_edit_token, get_page, edit_page, upload_file, move_page
 from infobox_updating import InfoboxUpdate, InfoboxType
 
-def update(testing=True):
+def update():
   api_url = 'https://wiki.factorio.com/api.php'
   session = requests.Session()
   edit_token = get_edit_token(session, api_url) 
@@ -32,7 +32,7 @@ def update(testing=True):
   # move pages + files - leave redirects - also do infoboxes on the pages
   for move_data in moves_and_more_data['move']:
     if 'Infobox' not in move_data['from'] and 'File' not in move_data['from']:
-      page = get_page(session, api_url, title)
+      page = get_page(session, api_url, move_data['from'])
       from_title_no_lang_suffix = re.search('([^/]+)(\/\S+)?', move_data['from']).group(1)
       to_title_no_lang_suffix = re.search('([^/]+)(\/\S+)?', move_data['to']).group(1)
       page = page.replace('{{:Infobox:' + from_title_no_lang_suffix + '}}', '{{:Infobox:' + to_title_no_lang_suffix + '}}')
@@ -59,14 +59,25 @@ def update(testing=True):
   # infobox update
   InfoboxUpdate([InfoboxType.Entity, InfoboxType.Technology, InfoboxType.Item, InfoboxType.Recipe, InfoboxType.Prototype], api_url, '0.17.0', False)
   
-  # what about the version history itself??
-  # latest version update ??? latest experimental is done outside of this function
-    # updating https://wiki.factorio.com/Template:VersionNav
-      # page = page.replace('}}\n<noinclude>', '|group10 = {{Translation|0.17}}\n|list10 =\n* {{TransLink|Version history/0.17.0#0.17.0|0.17.0}}\n}}\n<noinclude>')
-    # updating https://wiki.factorio.com/Main_Page/Latest_versions
-      # page = page.replace('[[File:Space science pack.png|link=]]', '[[File:Automation science pack.png|link=]]')
-      # page = page.replace('[[File:Speed module 3.png|link=]]', '[[File:Speed module.png|link=]]')
-      # page = page.replace('{{Translation|The wiki is based on version}} [[Version history/0.16.0|0.16]]', '{{Translation|The wiki is based on version}} [[Version history/0.17.0|0.17]]')
+  
+  # updating https://wiki.factorio.com/Template:VersionNav
+  versionnav = get_page(session, api_url, 'Template:VersionNav')
+  versionnav = versionnav.replace('}}\n<noinclude>', '|group10 = {{Translation|0.17}}\n|list10 =\n* {{TransLink|Version history/0.17.0#0.17.0|0.17.0}}\n}}\n<noinclude>')
+  print(edit_page(session, api_url, edit_token, 'Template:VersionNav', versionnav, '0.17').text)
+  
+  
+  # updating https://wiki.factorio.com/Main_Page/Latest_versions
+  latest_versions = get_page(session, api_url, 'Main_Page/Latest_versions')
+  latest_versions = latest_versions.replace('[[File:Space science pack.png|link=]]', '[[File:Automation science pack.png|link=]]')
+  latest_versions = latest_versions.replace('[[File:Speed module 3.png|link=]]', '[[File:Speed module.png|link=]]')
+  latest_versions = latest_versions.replace('{{Translation|The wiki is based on version}} [[Version history/0.16.0|0.16]]', '{{Translation|The wiki is based on version}} [[Version history/0.17.0|0.17]]')
+  print(edit_page(session, api_url, edit_token, 'Main_Page/Latest_versions', latest_versions, 'Experimental 0.17').text)
+  
+  
   # sitenotice https://wiki.factorio.com/MediaWiki:Sitenotice
-  # change/publish misc pages
-  # perhaps do some of the "manual" infobox work here?
+  sitenotice = "'''This wiki is about [[Tutorial:Cheat_sheet#0.17_change_overview|0.17]], the current [[Install_guide#Downloading_and_installing_experimental_versions|experimental version]] of ''Factorio''.'''\n\nInformation about 0.16, the current stable version of ''Factorio'', can be found on [https://stable.wiki.factorio.com/ stable.wiki.factorio.com]."
+  print(edit_page(session, api_url, edit_token, 'MediaWiki:Sitenotice', sitenotice, 'Experimental 0.17').text)
+
+  
+if __name__ == '__main__':
+  update()
