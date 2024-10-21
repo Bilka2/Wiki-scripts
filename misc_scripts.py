c9e6e1de301fdf70e2a19fae58479afb0d9c1d36
Bilka <Bilka2@users.noreply.github.com>
2024-10-21 13:24:45 +0200

    Added page moving script

--- misc_scripts.py
+++ misc_scripts.py
@@ -203,6 +203,162 @@ def move_archived_pages_to_namespace(): # https://forums.factorio.com/viewtopic.
     print(move_page(session, api_url, edit_token, title, move_target, 'Move archived pages to separate namespace').text)
 
 
+def two_point_zero():
+  session = requests.Session()
+  edit_token = get_edit_token(session, api_url)
+  
+  moves_and_more_data = {
+    "archive": [
+      "Rail signals (research)",
+      "Rail signals (research)/zh",
+      "Rail signals (research)/de",
+      "Rail signals (research)/ru",
+      "Rail signals (research)/uk",
+      "Rail signals (research)/ja",
+      "Stack filter inserter",
+      "Stack filter inserter/nl",
+      "Stack filter inserter/fr",
+      "Stack filter inserter/ko",
+      "Stack filter inserter/ru",
+      "Stack filter inserter/zh",
+      "Stack filter inserter/de",
+      "Stack filter inserter/uk",
+      "Stack filter inserter/pl",
+      "Stack filter inserter/ja",
+      "Filter inserter",
+      "Filter inserter/de",
+      "Filter inserter/nl",
+      "Filter inserter/zh",
+      "Filter inserter/uk",
+      "Filter inserter/ru",
+      "Filter inserter/fr",
+      "Filter inserter/pl",
+      "Filter inserter/ko",
+      "Filter inserter/ja",
+      "Filter inserter/pt-br",
+      "Rocket control unit (research)",
+      "Rocket control unit (research)/ja",
+      "Rocket control unit (research)/ru",
+      "Rocket control unit (research)/de",
+      "Rocket control unit (research)/zh",
+      "Rocket control unit (research)/uk",
+      "Rocket control unit",
+      "Rocket control unit/ko",
+      "Rocket control unit/fr",
+      "Rocket control unit/nl",
+      "Rocket control unit/de",
+      "Rocket control unit/es",
+      "Rocket control unit/uk",
+      "Rocket control unit/ru",
+      "Rocket control unit/zh",
+      "Rocket control unit/ja",
+      "Rocket control unit/it",
+      "Rocket control unit/pl",
+      "Rocket control unit/pt-br",
+    ],
+    "move": [
+      {"from": "Infobox:Portable fusion reactor", "to": "Infobox:Portable fission reactor"},
+      {"from": "Portable fusion reactor", "to": "Portable fission reactor"},
+      {"from": "Portable fusion reactor/zh", "to": "Portable fission reactor/zh"},
+      {"from": "Portable fusion reactor/de", "to": "Portable fission reactor/de"},
+      {"from": "Portable fusion reactor/uk", "to": "Portable fission reactor/uk"},
+      {"from": "Portable fusion reactor/ja", "to": "Portable fission reactor/ja"},
+      {"from": "Portable fusion reactor/ru", "to": "Portable fission reactor/ru"},
+      {"from": "Portable fusion reactor/pt-br", "to": "Portable fission reactor/pt-br"},
+      {"from": "Infobox:Portable fusion reactor (research)", "to": "Infobox:Portable fission reactor (research)"},
+      {"from": "Portable fusion reactor (research)", "to": "Portable fission reactor (research)"},
+      {"from": "Portable fusion reactor (research)/ru", "to": "Portable fission reactor (research)/ru"},
+      {"from": "Portable fusion reactor (research)/de", "to": "Portable fission reactor (research)/de"},
+      {"from": "Portable fusion reactor (research)/uk", "to": "Portable fission reactor (research)/uk"},
+      {"from": "Portable fusion reactor (research)/ja", "to": "Portable fission reactor (research)/ja"},
+      {"from": "Portable fusion reactor (research)/zh", "to": "Portable fission reactor (research)/zh"},
+      {"from": "Infobox:Stack inserter", "to": "Infobox:Bulk inserter"},
+      {"from": "Stack inserter", "to": "Bulk inserter"},
+      {"from": "Stack inserter/ja", "to": "Bulk inserter/ja"},
+      {"from": "Stack inserter/de", "to": "Bulk inserter/de"},
+      {"from": "Stack inserter/fr", "to": "Bulk inserter/fr"},
+      {"from": "Stack inserter/uk", "to": "Bulk inserter/uk"},
+      {"from": "Stack inserter/ko", "to": "Bulk inserter/ko"},
+      {"from": "Stack inserter/ru", "to": "Bulk inserter/ru"},
+      {"from": "Stack inserter/pl", "to": "Bulk inserter/pl"},
+      {"from": "Stack inserter/zh", "to": "Bulk inserter/zh"},
+      {"from": "Infobox:Stack inserter (research)", "to": "Infobox:Bulk inserter (research)"},
+      {"from": "Stack inserter (research)", "to": "Bulk inserter (research)"},
+      {"from": "Stack inserter (research)/zh", "to": "Bulk inserter (research)/zh"},
+      {"from": "Stack inserter (research)/ru", "to": "Bulk inserter (research)/ru"},
+      {"from": "Stack inserter (research)/es", "to": "Bulk inserter (research)/es"},
+      {"from": "Stack inserter (research)/uk", "to": "Bulk inserter (research)/uk"},
+      {"from": "Stack inserter (research)/ja", "to": "Bulk inserter (research)/ja"},
+      {"from": "Stack inserter (research)/de", "to": "Bulk inserter (research)/de"},
+      {"from": "Infobox:Advanced electronics (research)", "to": "Infobox:Advanced circuit (research)"},
+      {"from": "Advanced electronics (research)", "to": "Advanced circuit (research)"},
+      {"from": "Advanced electronics (research)/pl", "to": "Advanced circuit (research)/pl"},
+      {"from": "Advanced electronics (research)/zh", "to": "Advanced circuit (research)/zh"},
+      {"from": "Advanced electronics (research)/cs", "to": "Advanced circuit (research)/cs"},
+      {"from": "Advanced electronics (research)/uk", "to": "Advanced circuit (research)/uk"},
+      {"from": "Advanced electronics (research)/de", "to": "Advanced circuit (research)/de"},
+      {"from": "Advanced electronics (research)/ja", "to": "Advanced circuit (research)/ja"},
+      {"from": "Advanced electronics (research)/nl", "to": "Advanced circuit (research)/nl"},
+      {"from": "Advanced electronics (research)/ru", "to": "Advanced circuit (research)/ru"},
+      {"from": "Infobox:Advanced electronics 2 (research)", "to": "Infobox:Processing unit (research)"},
+      {"from": "Advanced electronics 2 (research)", "to": "Processing unit (research)"},
+      {"from": "Advanced electronics 2 (research)/zh", "to": "Processing unit (research)/zh"},
+      {"from": "Advanced electronics 2 (research)/cs", "to": "Processing unit (research)/cs"},
+      {"from": "Advanced electronics 2 (research)/ru", "to": "Processing unit (research)/ru"},
+      {"from": "Advanced electronics 2 (research)/uk", "to": "Processing unit (research)/uk"},
+      {"from": "Advanced electronics 2 (research)/ja", "to": "Processing unit (research)/ja"},
+      {"from": "Advanced electronics 2 (research)/pl", "to": "Processing unit (research)/pl"},
+      {"from": "Advanced electronics 2 (research)/de", "to": "Processing unit (research)/de"},
+      {"from": "Infobox:Used up uranium fuel cell", "to": "Infobox:Depleted uranium fuel cell"},
+      {"from": "Used up uranium fuel cell", "to": "Depleted uranium fuel cell"},
+      {"from": "Used up uranium fuel cell/fr", "to": "Depleted uranium fuel cell/fr"},
+      {"from": "Used up uranium fuel cell/ru", "to": "Depleted uranium fuel cell/ru"},
+      {"from": "Used up uranium fuel cell/zh", "to": "Depleted uranium fuel cell/zh"},
+      {"from": "Used up uranium fuel cell/ja", "to": "Depleted uranium fuel cell/ja"},
+      {"from": "Used up uranium fuel cell/de", "to": "Depleted uranium fuel cell/de"},
+      {"from": "Used up uranium fuel cell/pl", "to": "Depleted uranium fuel cell/pl"},
+      {"from": "Used up uranium fuel cell/ko", "to": "Depleted uranium fuel cell/ko"},
+      {"from": "Used up uranium fuel cell/uk", "to": "Depleted uranium fuel cell/uk"},
+      {"from": "Used up uranium fuel cell/nl", "to": "Depleted uranium fuel cell/nl"},
+      {"from": "Used up uranium fuel cell/es", "to": "Depleted uranium fuel cell/es"},
+      {"from": "Used up uranium fuel cell/pt-br", "to": "Depleted uranium fuel cell/pt-br"},
+      {"from": "Infobox:Optics (research)", "to": "Infobox:Lamp (research)"},
+      {"from": "Optics (research)", "to": "Lamp (research)"},
+      {"from": "Optics (research)/zh", "to": "Lamp (research)/zh"},
+      {"from": "Optics (research)/nl", "to": "Lamp (research)/nl"},
+      {"from": "Optics (research)/de", "to": "Lamp (research)/de"},
+      {"from": "Optics (research)/ru", "to": "Lamp (research)/ru"},
+      {"from": "Optics (research)/uk", "to": "Lamp (research)/uk"},
+      {"from": "Optics (research)/ja", "to": "Lamp (research)/ja"},
+      {"from": "Infobox:Energy weapons damage (research)", "to": "Infobox:Laser weapons damage (research)"},
+      {"from": "Energy weapons damage (research)", "to": "Laser weapons damage (research)"},
+      {"from": "Energy weapons damage (research)/de", "to": "Laser weapons damage (research)/de"},
+      {"from": "Energy weapons damage (research)/ru", "to": "Laser weapons damage (research)/ru"},
+      {"from": "Energy weapons damage (research)/zh", "to": "Laser weapons damage (research)/zh"},
+      {"from": "Energy weapons damage (research)/uk", "to": "Laser weapons damage (research)/uk"},
+      {"from": "Energy weapons damage (research)/ja", "to": "Laser weapons damage (research)/ja"}
+    ]
+  }
+  
+  # archive pages + files = prepend edit {{archive}} onto them and move to archive namespace
+  for title in moves_and_more_data['archive']:
+    print(edit_page(session, api_url, edit_token, title, '{{archive}}', 'Archived page (Removed in 2.0)', True).text) # prepend edit 
+    print(move_page(session, api_url, edit_token, title, f'Archive:{title}', 'Archived page (Removed in 2.0)').text)
+  
+  
+  # move pages + files - leave redirects - also do infoboxes on the pages
+  for move_data in moves_and_more_data['move']:
+    if 'Infobox' not in move_data['from'] and 'File' not in move_data['from']:
+      page = get_page(session, api_url, move_data['from'])
+      from_title_no_lang_suffix = re.search('([^/]+)(\/\S+)?', move_data['from']).group(1)
+      to_title_no_lang_suffix = re.search('([^/]+)(\/\S+)?', move_data['to']).group(1)
+      page = page.replace('{{:Infobox:' + from_title_no_lang_suffix + '}}', '{{:Infobox:' + to_title_no_lang_suffix + '}}')
+      
+      print(edit_page(session, api_url, edit_token, move_data['from'], page, 'Renamed in 2.0').text)
+      
+    print(move_page(session, api_url, edit_token, move_data['from'], move_data['to'], 'Renamed in 2.0').text)
+
+
 if __name__ == '__main__':
   # used_as_ammo_by_in_infobox(["Flamethrower turret"], "Light oil")
   
@@ -221,7 +377,9 @@ if __name__ == '__main__':
   # update_tech_icons()
   # update_icons()
   
-  categorize_images(["File:Yumako.png", "File:Yumako_seed.png", "File:Yumako_processing.png", "File:Yumako_mash.png", "File:Wood_processing.png", "File:Turbo_underground_belt.png", "File:Turbo_transport_belt.png", "File:Turbo_splitter.png", "File:Tungsten_ore.png", "File:Tree_seed.png", "File:Toolbelt_equipment.png", "File:Thruster.png", "File:Thruster_oxidizer.png", "File:Thruster_fuel.png", "File:Tesla_gun.png", "File:Tesla_ammo.png", "File:Supercapacitor.png", "File:Steam_condensation.png", "File:Spoilage.png", "File:Space_platform_hub.png", "File:Space_platform_foundation.png", "File:Solid_fuel_from_ammonia.png", "File:Simple_coal_liquefaction.png", "File:Selector_combinator.png", "File:Scrap_recycling.png", "File:Rocket_turret.png", "File:Rocket_fuel_from_jelly.png", "File:Railgun_ammo.png", "File:Rail_support.png", "File:Rail_ramp.png", "File:Quality_module.png", "File:Quality_module_3.png", "File:Quality_module_2.png", "File:Promethium_asteroid_chunk.png", "File:Portable_fission_reactor.png", "File:Plasma.png", "File:Personal_battery_MK3.png", "File:Pentapod_egg.png", "File:Oxide_asteroid_reprocessing.png", "File:Oxide_asteroid_crushing.png", "File:Oxide_asteroid_chunk.png", "File:Overgrowth_yumako_soil.png", "File:Overgrowth_jellynut_soil.png", "File:Nutrients.png", "File:Nutrients_from_yumako_mash.png", "File:Nutrients_from_spoilage.png", "File:Nutrients_from_fish.png", "File:Nutrients_from_biter_egg.png", "File:Nutrients_from_bioflux.png", "File:Molten_iron_from_lava.png", "File:Molten_copper.png", "File:Molten_copper_from_lava.png", "File:Metallic_asteroid_reprocessing.png", "File:Metallic_asteroid_crushing.png", "File:Metallic_asteroid_chunk.png", "File:Mech_armor.png", "File:Lithium.png", "File:Lithium_brine.png", "File:Lightning_rod.png", "File:Lightning_collector.png", "File:Jellynut.png", "File:Jellynut_seed.png", "File:Jellynut_processing.png", "File:Jelly.png", "File:Iron_bacteria.png", "File:Iron_bacteria_cultivation.png", "File:Ice_platform.png", "File:Ice_melting.png", "File:Holmium_solution.png", "File:Holmium_plate.png", "File:Heating_tower.png", "File:Fusion_reactor.png", "File:Fusion_power_cell.png", "File:Fusion_generator.png", "File:Foundation.png", "File:Fluoroketone.png", "File:Fluoroketone_cooling.png", "File:Fluoroketone_(hot)_barrel.png", "File:Fluoroketone_(cold)_barrel.png", "File:Fluorine.png", "File:Fish_breeding.png", "File:Empty_fluoroketone_(hot)_barrel.png", "File:Empty_fluoroketone_(cold)_barrel.png", "File:Electrolyte.png", "File:Display_panel.png", "File:Crusher.png", "File:Copper_bacteria.png", "File:Copper_bacteria_cultivation.png", "File:Concrete_from_molten_iron.png", "File:Coal_synthesis.png", "File:Casting_steel.png", "File:Casting_pipe.png", "File:Casting_pipe_to_ground.png", "File:Casting_low_density_structure.png", "File:Casting_iron.png", "File:Casting_iron_stick.png", "File:Casting_iron_gear_wheel.png", "File:Casting_copper.png", "File:Casting_copper_cable.png", "File:Cargo_bay.png", "File:Carbonic_asteroid_reprocessing.png", "File:Carbonic_asteroid_crushing.png", "File:Carbonic_asteroid_chunk.png", "File:Capture_bot_rocket.png", "File:Captive_biter_spawner.png", "File:Calcite.png", "File:Burnt_spoilage.png", "File:Biter_egg.png", "File:Biosulfur.png", "File:Bioplastic.png", "File:Biolubricant.png", "File:Biolab.png", "File:Bioflux.png", "File:Biochamber.png", "File:Asteroid_collector.png", "File:Artificial_yumako_soil.png", "File:Artificial_jellynut_soil.png", "File:Ammoniacal_solution.png", "File:Ammoniacal_solution_separation.png", "File:Ammonia.png", "File:Ammonia_rocket_fuel.png", "File:Advanced_thruster_oxidizer.png", "File:Advanced_thruster_fuel.png", "File:Advanced_oxide_asteroid_crushing.png", "File:Advanced_metallic_asteroid_crushing.png", "File:Advanced_carbonic_asteroid_crushing.png", "File:Acid_neutralisation.png"])
+  two_point_zero()
+  
+  # categorize_images(["File:Yumako.png", "File:Yumako_seed.png", "File:Yumako_processing.png", "File:Yumako_mash.png", "File:Wood_processing.png", "File:Turbo_underground_belt.png", "File:Turbo_transport_belt.png", "File:Turbo_splitter.png", "File:Tungsten_ore.png", "File:Tree_seed.png", "File:Toolbelt_equipment.png", "File:Thruster.png", "File:Thruster_oxidizer.png", "File:Thruster_fuel.png", "File:Tesla_gun.png", "File:Tesla_ammo.png", "File:Supercapacitor.png", "File:Steam_condensation.png", "File:Spoilage.png", "File:Space_platform_hub.png", "File:Space_platform_foundation.png", "File:Solid_fuel_from_ammonia.png", "File:Simple_coal_liquefaction.png", "File:Selector_combinator.png", "File:Scrap_recycling.png", "File:Rocket_turret.png", "File:Rocket_fuel_from_jelly.png", "File:Railgun_ammo.png", "File:Rail_support.png", "File:Rail_ramp.png", "File:Quality_module.png", "File:Quality_module_3.png", "File:Quality_module_2.png", "File:Promethium_asteroid_chunk.png", "File:Portable_fission_reactor.png", "File:Plasma.png", "File:Personal_battery_MK3.png", "File:Pentapod_egg.png", "File:Oxide_asteroid_reprocessing.png", "File:Oxide_asteroid_crushing.png", "File:Oxide_asteroid_chunk.png", "File:Overgrowth_yumako_soil.png", "File:Overgrowth_jellynut_soil.png", "File:Nutrients.png", "File:Nutrients_from_yumako_mash.png", "File:Nutrients_from_spoilage.png", "File:Nutrients_from_fish.png", "File:Nutrients_from_biter_egg.png", "File:Nutrients_from_bioflux.png", "File:Molten_iron_from_lava.png", "File:Molten_copper.png", "File:Molten_copper_from_lava.png", "File:Metallic_asteroid_reprocessing.png", "File:Metallic_asteroid_crushing.png", "File:Metallic_asteroid_chunk.png", "File:Mech_armor.png", "File:Lithium.png", "File:Lithium_brine.png", "File:Lightning_rod.png", "File:Lightning_collector.png", "File:Jellynut.png", "File:Jellynut_seed.png", "File:Jellynut_processing.png", "File:Jelly.png", "File:Iron_bacteria.png", "File:Iron_bacteria_cultivation.png", "File:Ice_platform.png", "File:Ice_melting.png", "File:Holmium_solution.png", "File:Holmium_plate.png", "File:Heating_tower.png", "File:Fusion_reactor.png", "File:Fusion_power_cell.png", "File:Fusion_generator.png", "File:Foundation.png", "File:Fluoroketone.png", "File:Fluoroketone_cooling.png", "File:Fluoroketone_(hot)_barrel.png", "File:Fluoroketone_(cold)_barrel.png", "File:Fluorine.png", "File:Fish_breeding.png", "File:Empty_fluoroketone_(hot)_barrel.png", "File:Empty_fluoroketone_(cold)_barrel.png", "File:Electrolyte.png", "File:Display_panel.png", "File:Crusher.png", "File:Copper_bacteria.png", "File:Copper_bacteria_cultivation.png", "File:Concrete_from_molten_iron.png", "File:Coal_synthesis.png", "File:Casting_steel.png", "File:Casting_pipe.png", "File:Casting_pipe_to_ground.png", "File:Casting_low_density_structure.png", "File:Casting_iron.png", "File:Casting_iron_stick.png", "File:Casting_iron_gear_wheel.png", "File:Casting_copper.png", "File:Casting_copper_cable.png", "File:Cargo_bay.png", "File:Carbonic_asteroid_reprocessing.png", "File:Carbonic_asteroid_crushing.png", "File:Carbonic_asteroid_chunk.png", "File:Capture_bot_rocket.png", "File:Captive_biter_spawner.png", "File:Calcite.png", "File:Burnt_spoilage.png", "File:Biter_egg.png", "File:Biosulfur.png", "File:Bioplastic.png", "File:Biolubricant.png", "File:Biolab.png", "File:Bioflux.png", "File:Biochamber.png", "File:Asteroid_collector.png", "File:Artificial_yumako_soil.png", "File:Artificial_jellynut_soil.png", "File:Ammoniacal_solution.png", "File:Ammoniacal_solution_separation.png", "File:Ammonia.png", "File:Ammonia_rocket_fuel.png", "File:Advanced_thruster_oxidizer.png", "File:Advanced_thruster_fuel.png", "File:Advanced_oxide_asteroid_crushing.png", "File:Advanced_metallic_asteroid_crushing.png", "File:Advanced_carbonic_asteroid_crushing.png", "File:Acid_neutralisation.png"])
   
   # dump_pages(['Types/ActivateEquipmentCapsuleAction', 'Types/ActivityBarStyleSpecification', 'Types/AmmoDamageModifierPrototype'])
   
